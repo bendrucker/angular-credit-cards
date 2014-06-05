1ee45d6a20bce97b149b4094c46033cd7aa8238d
Ben Drucker <bvdrucker@gmail.com>
2014-06-05 07:49:58 -0400

    use an ngModel controller directly instead of a wrapping form

--- test/number.js
+++ test/number.js
@@ -6,26 +6,23 @@ describe('cc-number', function () {
 
   beforeEach(angular.mock.module('credit-cards'));
 
-  var creditCard, scope;
+  var creditCard, scope, controller;
   beforeEach(angular.mock.inject(function ($injector) {
     var $compile = $injector.get('$compile');
-    var element  = angular.element(
-      '<form name="form">' +
-        '<input ng-model="card.number" cc-number name="number" />' +
-      '</form>'
-    );
+    var element  = angular.element('<input ng-model="card.number" cc-number />');
     creditCard   = $injector.get('creditCard');
     scope        = $injector.get('$rootScope').$new();
     scope.card   = {};
     $compile(element)(scope);
+    controller   = element.controller('ngModel');
   }));
 
   it('is valid when a card validation is truthy', function () {
     sinon.stub(creditCard, 'validate').returns(false);
-    scope.form.number.$setViewValue(' ');
+    controller.$setViewValue(' ');
     scope.$digest();
     expect(scope.card.number).to.be.undefined;
-    expect(scope.form.number.$valid).to.be.false;
+    expect(controller.$valid).to.be.false;
   });
 
   xit('is invalid when card validation is falsy', function () {
