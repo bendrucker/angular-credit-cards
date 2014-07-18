e824b0e89ad200752d688b745099208728bc774b
Ben Drucker <bvdrucker@gmail.com>
2014-07-18 19:06:51 -0400

    Add a numeric pattern and validate against specific card types

--- src/cvc.js
+++ src/cvc.js
@@ -6,13 +6,20 @@ module.exports = function () {
   return {
     restrict: 'A',
     require: 'ngModel',
+    scope: {
+      ccType: '='
+    },
     compile: function (element, attributes) {
       attributes.$set('maxlength', 4);
+      attributes.$set('pattern', '[0-9]*');
       return function (scope, element, attributes, ngModelController) {
         ngModelController.$parsers.unshift(function (value) {
-          var valid = cvc.isValid(value);
+          var valid = cvc.isValid(value, scope.ccType);
           ngModelController.$setValidity('ccCvc', valid);
           if (valid) return value;
+        });
+        scope.$watch('ccType', function () {
+          ngModelController.$setViewValue(ngModelController.$viewValue);
         });
       };
     }
--- test/cvc.js
+++ test/cvc.js
@@ -9,12 +9,16 @@ describe('cc-cvc', function () {
     $compile   = $injector.get('$compile');
     scope      = $injector.get('$rootScope').$new();
     scope.card = {};
-    element    = angular.element('<input ng-model="card.cvc" cc-cvc />');
+    element    = angular.element('<input ng-model="card.cvc" cc-cvc cc-type="cardType" />');
     controller = $compile(element)(scope).controller('ngModel');
   }));
 
   it('sets maxlength to 4', function () {
     expect(element.attr('maxlength')).to.equal('4');
+  });
+
+  it('adds a numeric pattern', function () {
+    expect(element.attr('pattern')).to.equal('[0-9]*');
   });
 
   it('accepts a 3 digit numeric', function () {
@@ -24,11 +28,22 @@ describe('cc-cvc', function () {
     expect(scope.card.cvc).to.equal('123');
   });
 
-  it('accepts a 4 digit numeric string', function () {
+  it('accepts a 4 digit numeric', function () {
     controller.$setViewValue('1234');
     scope.$digest();
     expect(controller.$valid).to.be.true;
     expect(scope.card.cvc).to.equal('1234');
+  });
+
+  it('can validate against the card type', function () {
+    scope.cardType = 'visa';
+    controller.$setViewValue('1234');
+    scope.$digest();
+    expect(controller.$valid).to.be.false;
+    scope.cardType = 'americanExpress';
+    scope.$digest();
+
+    expect(controller.$valid).to.be.true;
   });
 
   it('does not accept numbers', function () {
