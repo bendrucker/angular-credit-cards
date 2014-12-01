acb949b00d87b484958a6d4df0cf98ecfb2f1152
Ben Drucker <bvdrucker@gmail.com>
2014-11-30 19:57:56 -0500

    Simplify module registration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
-require('angular')
+module.exports = require('angular')
   .module('credit-cards', [])
   .value('creditcards', require('creditcards'))
   .directive('ccNumber', require('./number'))
   .directive('ccExp', require('./expiration'))
   .directive('ccExpMonth', require('./expiration').month)
   .directive('ccExpYear', require('./expiration').year)
-  .directive('ccCvc', require('./cvc'));
-
-module.exports = 'credit-cards';
+  .directive('ccCvc', require('./cvc'))
+  .name;
