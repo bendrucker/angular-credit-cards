a16892d3a3dded7c07a01b364585f9fbdc52d170
Ben Drucker <bvdrucker@gmail.com>
2016-01-19 06:56:38 -0800

    UMD Build

--- release/angular-credit-cards.js
+++ release/angular-credit-cards.js
@@ -67,50 +67,21 @@ module.exports = function (string) {
     });
 };
 
-},{"sentence-case":3}],3:[function(_dereq_,module,exports){
-/**
- * Sentence case a string.
- *
- * @param  {String} string
- * @return {String}
- */
-module.exports = function (string) {
-  return String(string)
-    // Add camel case support.
-    .replace(/([a-z])([A-Z0-9])/g, '$1 $2')
-    // Remove every non-word character and replace with a period.
-    .replace(/[^a-zA-Z0-9]+/g, '.')
-    // Replace every period not between two numbers with a space.
-    .replace(/(?!\d\.\d)(^|.)\./g, '$1 ')
-    // Trim whitespace from the string.
-    .replace(/^ | $/g, '')
-    // Finally lower case the entire string.
-    .toLowerCase();
-};
+},{"sentence-case":14}],3:[function(_dereq_,module,exports){
+'use strict'
 
-},{}],4:[function(_dereq_,module,exports){
-module.exports = extend
+var isArray = _dereq_('isarray')
 
-function extend(target) {
-    for (var i = 1; i < arguments.length; i++) {
-        var source = arguments[i]
-
-        for (var key in source) {
-            if (source.hasOwnProperty(key)) {
-                target[key] = source[key]
-            }
-        }
-    }
-
-    return target
+module.exports = function castArray (value) {
+  return isArray(value) ? value : [value]
 }
 
-},{}],5:[function(_dereq_,module,exports){
+},{"isarray":13}],4:[function(_dereq_,module,exports){
 'use strict';
 
 exports.types = _dereq_('./types');
 
-},{"./types":7}],6:[function(_dereq_,module,exports){
+},{"./types":6}],5:[function(_dereq_,module,exports){
 'use strict';
 
 var extend = _dereq_('xtend/mutable');
@@ -141,7 +112,7 @@ function identity (value) {
   return value;
 }
 
-},{"xtend/mutable":4}],7:[function(_dereq_,module,exports){
+},{"xtend/mutable":15}],6:[function(_dereq_,module,exports){
 'use strict';
 
 var Type = _dereq_('./type');
@@ -204,27 +175,7 @@ exports.unionPay = new Type('UnionPay', {
   luhn: false
 });
 
-},{"./type":6}],8:[function(_dereq_,module,exports){
-'use strict'
-
-module.exports = (function (array) {
-  return function luhn (number) {
-    if (!number) return false
-    var length = number.length
-    var bit = 1
-    var sum = 0
-    var value
-
-    while (length) {
-      value = parseInt(number.charAt(--length), 10)
-      sum += (bit ^= 1) ? array[value] : value
-    }
-
-    return !!sum && sum % 10 === 0
-  }
-}([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]))
-
-},{}],9:[function(_dereq_,module,exports){
+},{"./type":5}],7:[function(_dereq_,module,exports){
 'use strict'
 
 var camel = _dereq_('camel-case')
@@ -264,7 +215,7 @@ function getType (number, eager) {
   }
 }
 
-},{"camel-case":2,"creditcards-types":5,"fast-luhn":8}],10:[function(_dereq_,module,exports){
+},{"camel-case":2,"creditcards-types":4,"fast-luhn":11}],8:[function(_dereq_,module,exports){
 'use strict'
 
 var camel = _dereq_('camel-case')
@@ -279,7 +230,7 @@ exports.isValid = function (cvc, type) {
   return card.types[camel(type)].cvcLength === cvc.length
 }
 
-},{"./card":9,"camel-case":2}],11:[function(_dereq_,module,exports){
+},{"./card":7,"camel-case":2}],9:[function(_dereq_,module,exports){
 'use strict'
 
 exports.isPast = function (month, year) {
@@ -321,7 +272,7 @@ exports.year = {
   }
 }
 
-},{}],12:[function(_dereq_,module,exports){
+},{}],10:[function(_dereq_,module,exports){
 'use strict'
 
 var card = exports.card = _dereq_('./card')
@@ -345,7 +296,27 @@ exports.validate = function (cardObj) {
   }
 }
 
-},{"./card":9,"./cvc":10,"./expiration":11}],13:[function(_dereq_,module,exports){
+},{"./card":7,"./cvc":8,"./expiration":9}],11:[function(_dereq_,module,exports){
+'use strict'
+
+module.exports = (function (array) {
+  return function luhn (number) {
+    if (!number) return false
+    var length = number.length
+    var bit = 1
+    var sum = 0
+    var value
+
+    while (length) {
+      value = parseInt(number.charAt(--length), 10)
+      sum += (bit ^= 1) ? array[value] : value
+    }
+
+    return !!sum && sum % 10 === 0
+  }
+}([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]))
+
+},{}],12:[function(_dereq_,module,exports){
 var ERROR_MESSAGE = 'Function.prototype.bind called on incompatible ';
 var slice = Array.prototype.slice;
 var toStr = Object.prototype.toString;
@@ -395,7 +366,52 @@ module.exports = function bind(that) {
 };
 
 
+},{}],13:[function(_dereq_,module,exports){
+module.exports = Array.isArray || function (arr) {
+  return Object.prototype.toString.call(arr) == '[object Array]';
+};
+
 },{}],14:[function(_dereq_,module,exports){
+/**
+ * Sentence case a string.
+ *
+ * @param  {String} string
+ * @return {String}
+ */
+module.exports = function (string) {
+  return String(string)
+    // Add camel case support.
+    .replace(/([a-z])([A-Z0-9])/g, '$1 $2')
+    // Remove every non-word character and replace with a period.
+    .replace(/[^a-zA-Z0-9]+/g, '.')
+    // Replace every period not between two numbers with a space.
+    .replace(/(?!\d\.\d)(^|.)\./g, '$1 ')
+    // Trim whitespace from the string.
+    .replace(/^ | $/g, '')
+    // Finally lower case the entire string.
+    .toLowerCase();
+};
+
+},{}],15:[function(_dereq_,module,exports){
+module.exports = extend
+
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+function extend(target) {
+    for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i]
+
+        for (var key in source) {
+            if (hasOwnProperty.call(source, key)) {
+                target[key] = source[key]
+            }
+        }
+    }
+
+    return target
+}
+
+},{}],16:[function(_dereq_,module,exports){
 'use strict'
 
 var cvc = _dereq_('creditcards').cvc
@@ -426,7 +442,7 @@ function factory ($parse) {
   }
 }
 
-},{"creditcards":12,"function-bind":13}],15:[function(_dereq_,module,exports){
+},{"creditcards":10,"function-bind":12}],17:[function(_dereq_,module,exports){
 'use strict'
 
 var expiration = _dereq_('creditcards').expiration
@@ -539,10 +555,12 @@ exports.year = function ccExpYear () {
 
 function noop () {}
 
-},{"ap":1,"creditcards":12}],16:[function(_dereq_,module,exports){
+},{"ap":1,"creditcards":10}],18:[function(_dereq_,module,exports){
 'use strict'
 
 var card = _dereq_('creditcards').card
+var array = _dereq_('cast-array')
+var partial = _dereq_('ap').partial
 
 module.exports = factory
 
@@ -571,6 +589,16 @@ function factory ($parse) {
           return ngModel.$viewValue
         }
 
+        function setCursorPostion (element, position) {
+          if (element.setSelectionRange) {
+            element.setSelectionRange(position, position)
+          } else if (element.createTextRange) {
+            var range = element.createTextRange()
+            range.move('character', position)
+            range.select()
+          }
+        }
+
         if ($attributes.ccEagerType != null) {
           $scope.$watch($viewValue, function eagerTypeCheck (number) {
             if (!number) return
@@ -586,22 +614,25 @@ function factory ($parse) {
         }
 
         if ($attributes.ccFormat != null) {
-          $scope.$watch($viewValue, function formatInput (input, previous) {
+          $element.on('input', function formatInput () {
+            var input = $element.val()
+            var previous = $viewValue()
             if (!input) return
             var element = $element[0]
             var formatted = card.format(card.parse(input))
 
-            ngModel.$setViewValue(formatted)
             var selectionEnd = element.selectionEnd
+            ngModel.$setViewValue(formatted)
             ngModel.$render()
-            if (formatted && !formatted.charAt(selectionEnd - 1).trim()) {
-              if (previous && previous.length < input.length) {
+            if (formatted && (!formatted.charAt(selectionEnd - 1).trim())) {
+              if (previous && previous.length < formatted.length) {
                 selectionEnd++
               } else {
                 selectionEnd--
               }
             }
-            element.setSelectionRange(selectionEnd, selectionEnd)
+            setCursorPostion(element, selectionEnd)
+
           })
         }
 
@@ -612,18 +643,20 @@ function factory ($parse) {
         }
 
         ngModel.$validators.ccNumberType = function validateCcNumberType (number) {
-          return card.isValid(number, $parse($attributes.ccType)($scope))
+          var type = $parse($attributes.ccType)($scope)
+          if (!type) card.isValid(number)
+          return array(type).some(partial(card.isValid, number))
         }
       }
     }
   }
 }
 
-},{"creditcards":12}],17:[function(_dereq_,module,exports){
+},{"ap":1,"cast-array":3,"creditcards":10}],19:[function(_dereq_,module,exports){
 (function (global){
 'use strict'
 
-var angular = (typeof window !== "undefined" ? window.angular : typeof global !== "undefined" ? global.angular : null)
+var angular = (typeof window !== "undefined" ? window['angular'] : typeof global !== "undefined" ? global['angular'] : null)
 var creditcards = _dereq_('creditcards')
 var number = _dereq_('./number')
 var cvc = _dereq_('./cvc')
@@ -640,5 +673,5 @@ module.exports = angular
   .name
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./cvc":14,"./expiration":15,"./number":16,"creditcards":12}]},{},[17])(17)
+},{"./cvc":16,"./expiration":17,"./number":18,"creditcards":10}]},{},[19])(19)
 });
