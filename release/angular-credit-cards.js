9921fe953f10083c5416d045d530ded0d02c006e
Ben Drucker <bvdrucker@gmail.com>
2018-02-22 08:49:53 -0800

    UMD Build

--- release/angular-credit-cards.js
+++ release/angular-credit-cards.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.angularCreditCards = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.angularCreditCards = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(_dereq_,module,exports){
 exports = module.exports = ap;
 function ap (args, fn) {
     return function () {
@@ -842,7 +842,6 @@ function factory ($parse, $timeout) {
           ngModel.$formatters.unshift(card.format)
           $element.on('input', function formatInput () {
             var input = $element.val()
-            var previous = $viewValue()
             if (!input) return
             var element = $element[0]
             var formatted = card.format(card.parse(input))
@@ -851,7 +850,7 @@ function factory ($parse, $timeout) {
             ngModel.$setViewValue(formatted)
             ngModel.$render()
 
-            if (previous && selectionEnd === previous.length && previous.length < formatted.length) {
+            if (selectionEnd === input.length && input.length < formatted.length) {
               selectionEnd = formatted.length
             }
             setCursorPostion(element, selectionEnd)
